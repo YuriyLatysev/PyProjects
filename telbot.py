b049b5c7e55d19c1117eabbf9354a405d3b7fb52
YriyLatysev <ulatysev9@gmail.com>
2023-02-18 00:59:58 +0200

    Begin

--- telbot.py
+++ telbot.py
@@ -2,6 +2,7 @@ import csv
 import random
 
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('5901360246:AAE34FfQ-FaM2UCVv1t4lrlEIdAHLqssea0')
 
@@ -15,7 +16,7 @@ def start(message):
 
 @bot.message_handler(commands=['help'])
 def help_bot(message):
-    bot.send_message(message.chat.id, 'Help')
+    bot.send_message(message.chat.id, 'Бот предназначен для изучения английских слов.')
 
 
 def check_id(user_id, message_id):
@@ -25,17 +26,38 @@ def check_id(user_id, message_id):
             for row in reader:
                 if row['user_id'] == str(user_id):
                     bot.send_message(message_id, f'Вы выбрали изучать {row["num_words"]} в день')
-                    return row['num_words']
+                    # return row['num_words']
                     break
             else:
-                bot.send_message(message_id, 'Сколько слов в день вы хотите изучать?')
-                with open('id.csv', 'w', newline='') as csv_file_wr:
-                    fieldnames = ['user_id', 'num_words']
-                    writer = csv.DictWriter(csv_file_wr, fieldnames=fieldnames)
-                    writer.writerow({'user_id': user_id, 'num_words':'1'})
-                    return 1
+                choose_num_words(message_id, 'Сколько слов в день вы хотите изучать?', user_id)
+                    # return 1
     except FileNotFoundError as err:
         print(f'File does not exist. error - {err}')
+
+
+def choose_num_words(message_id, mes, user_id):
+    markup = types.InlineKeyboardMarkup(row_width=5)
+    but1 = types.InlineKeyboardButton('1', callback_data=str(user_id) + ':1')
+    but2 = types.InlineKeyboardButton('2', callback_data=str(user_id) + ':2')
+    but3 = types.InlineKeyboardButton('3', callback_data=str(user_id) + ':3')
+    but4 = types.InlineKeyboardButton('4', callback_data=str(user_id) + ':4')
+    but5 = types.InlineKeyboardButton('5', callback_data=str(user_id) + ':5')
+    but6 = types.InlineKeyboardButton('6', callback_data=str(user_id) + ':6')
+    but7 = types.InlineKeyboardButton('7', callback_data=str(user_id) + ':7')
+    but8 = types.InlineKeyboardButton('8', callback_data=str(user_id) + ':8')
+    but9 = types.InlineKeyboardButton('9', callback_data=str(user_id) + ':9')
+    but10 = types.InlineKeyboardButton('10', callback_data=str(user_id) + ':10')
+    markup.add(but1, but2, but3, but4, but5, but6, but7, but8, but9, but10)
+    bot.send_message(message_id, mes, reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def querry_handler(call):
+    num_words = call.data.split(":")[1]
+    user_id = call.data.split(":")[0]
+    with open('id.csv', 'a', newline='') as csv_file_wr:
+        fieldnames = ['user_id', 'num_words']
+        writer = csv.DictWriter(csv_file_wr, fieldnames=fieldnames)
+        writer.writerow({'user_id': user_id, 'num_words': num_words})
 
 
 def choose_word():
