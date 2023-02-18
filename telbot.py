8c4eb2d52363779c05fffad251da4b987c3b4ca6
YriyLatysev <ulatysev9@gmail.com>
2023-02-18 15:01:47 +0200

    almost done

--- telbot.py
+++ telbot.py
@@ -26,11 +26,9 @@ def check_id(user_id, message_id):
             for row in reader:
                 if row['user_id'] == str(user_id):
                     bot.send_message(message_id, f'Вы выбрали изучать {row["num_words"]} в день')
-                    # return row['num_words']
                     break
             else:
                 choose_num_words(message_id, 'Сколько слов в день вы хотите изучать?', user_id)
-                    # return 1
     except FileNotFoundError as err:
         print(f'File does not exist. error - {err}')
 
@@ -52,18 +50,32 @@ def choose_num_words(message_id, mes, user_id):
 
 @bot.callback_query_handler(func=lambda call: True)
 def querry_handler(call):
-    num_words = call.data.split(":")[1]
-    user_id = call.data.split(":")[0]
-    with open('id.csv', 'a', newline='') as csv_file_wr:
+    temp = list()
+    num_words_new = call.data.split(":")[1]
+    user_id_new = call.data.split(":")[0]
+    with open('id.csv', 'r', newline='', encoding='utf-8') as csv_file_r:
+        reader = csv.DictReader(csv_file_r)
+        for row in reader:
+            temp.append(row)
+    with open('id.csv', 'w', newline='', encoding='utf-8') as csv_file_wr:
         fieldnames = ['user_id', 'num_words']
         writer = csv.DictWriter(csv_file_wr, fieldnames=fieldnames)
-        writer.writerow({'user_id': user_id, 'num_words': num_words})
+        writer.writerow({'user_id': 'user_id', 'num_words': 'num_words'})
+        new = True
+        for row in temp:
+            if row['user_id'] == str(user_id_new):
+                writer.writerow({'user_id': row['user_id'], 'num_words': num_words_new})
+                new = False
+            else:
+                writer.writerow({'user_id': row['user_id'], 'num_words': row['num_words']})
+        if new:
+            writer.writerow({'user_id': user_id_new, 'num_words': num_words_new})
 
 
 def choose_word():
     try:
         with open('words.csv', 'r', newline='', encoding='utf-8') as csv_file:
-            rand_num = random.randint(1, 5000)
+            rand_num = random.randint(1, 4654)
             reader = csv.DictReader(csv_file)
             for row in reader:
                 if row['num'] == str(rand_num):
@@ -72,7 +84,11 @@ def choose_word():
         print(f'File does not exist. error - {err}')
         return None, None, None, None
 
+def main():
+    english_word, russian_word1, russian_word2, russian_word3 = choose_word()
+    with open()
+
 
 if __name__ == '__main__':
-    english_word, russian_word1, russian_word2, russian_word3 = choose_word()
+    # english_word, russian_word1, russian_word2, russian_word3 = choose_word()
     bot.polling(none_stop=True)
