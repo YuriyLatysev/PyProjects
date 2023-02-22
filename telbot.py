345b2745bf52f9561b1049f719853ec50ec7182c
YriyLatysev <ulatysev9@gmail.com>
2023-02-22 17:17:18 +0200

    End

--- telbot.py
+++ telbot.py
@@ -1,6 +1,5 @@
 import csv
 import random
-
 import telebot
 from telebot import types
 
@@ -10,7 +9,8 @@ bot = telebot.TeleBot('5901360246:AAE34FfQ-FaM2UCVv1t4lrlEIdAHLqssea0')
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, f'<b>Привет, {message.from_user.first_name}</b>', parse_mode='html')
-    bot.send_message(message.chat.id, f'<b>Бот будет присылать вам слова, вы должны написать перевод</b>', parse_mode='html')
+    bot.send_message(message.chat.id, f'<b>Бот будет присылать вам слова, вы должны написать перевод</b>',
+                     parse_mode='html')
     check_id(message.from_user.id, message.chat.id)
 
 
@@ -20,39 +20,62 @@ def help_bot(message):
 
 
 def check_id(user_id, message_id):
+    new = True
     try:
         with open('id.csv', 'r', newline='') as csv_file:
             reader = csv.DictReader(csv_file)
             for row in reader:
                 if row['user_id'] == str(user_id):
                     bot.send_message(message_id, f'Вы выбрали изучать {row["num_words"]} в день')
-                    break
+                    markup = types.InlineKeyboardMarkup(row_width=2)
+                    but1 = types.InlineKeyboardButton('Да', callback_data='ans_1:' + str(user_id) + ':Yes')
+                    but2 = types.InlineKeyboardButton('Нет', callback_data='ans_1:' + str(user_id) + ':No')
+                    markup.add(but1, but2)
+                    bot.send_message(message_id, 'Хотите изменить количество слов?', reply_markup=markup)
+                    new = False
             else:
-                choose_num_words(message_id, 'Сколько слов в день вы хотите изучать?', user_id)
+                if new:
+                    choose_num_words(message_id, 'Сколько слов в день вы хотите изучать?', user_id)
     except FileNotFoundError as err:
         print(f'File does not exist. error - {err}')
 
 
+@bot.callback_query_handler(func=lambda call: call.data.split(":")[0] == "ans_1")
+def querry_handler_1(call):
+    answer = call.data.split(':')[2]
+    user_id_new = call.data.split(":")[1]
+    bot.edit_message_reply_markup(user_id_new, call.message.message_id, reply_markup=None)
+    if answer == 'Yes':
+        choose_num_words(user_id_new, 'Сколько слов в день вы хотите изучать?', user_id_new)
+
+
 def choose_num_words(message_id, mes, user_id):
     markup = types.InlineKeyboardMarkup(row_width=5)
-    but1 = types.InlineKeyboardButton('1', callback_data=str(user_id) + ':1')
-    but2 = types.InlineKeyboardButton('2', callback_data=str(user_id) + ':2')
-    but3 = types.InlineKeyboardButton('3', callback_data=str(user_id) + ':3')
-    but4 = types.InlineKeyboardButton('4', callback_data=str(user_id) + ':4')
-    but5 = types.InlineKeyboardButton('5', callback_data=str(user_id) + ':5')
-    but6 = types.InlineKeyboardButton('6', callback_data=str(user_id) + ':6')
-    but7 = types.InlineKeyboardButton('7', callback_data=str(user_id) + ':7')
-    but8 = types.InlineKeyboardButton('8', callback_data=str(user_id) + ':8')
-    but9 = types.InlineKeyboardButton('9', callback_data=str(user_id) + ':9')
-    but10 = types.InlineKeyboardButton('10', callback_data=str(user_id) + ':10')
+    but1 = types.InlineKeyboardButton('1', callback_data='ans:' + str(user_id) + ':1')
+    but2 = types.InlineKeyboardButton('2', callback_data='ans:' + str(user_id) + ':2')
+    but3 = types.InlineKeyboardButton('3', callback_data='ans:' + str(user_id) + ':3')
+    but4 = types.InlineKeyboardButton('4', callback_data='ans:' + str(user_id) + ':4')
+    but5 = types.InlineKeyboardButton('5', callback_data='ans:' + str(user_id) + ':5')
+    but6 = types.InlineKeyboardButton('6', callback_data='ans:' + str(user_id) + ':6')
+    but7 = types.InlineKeyboardButton('7', callback_data='ans:' + str(user_id) + ':7')
+    but8 = types.InlineKeyboardButton('8', callback_data='ans:' + str(user_id) + ':8')
+    but9 = types.InlineKeyboardButton('9', callback_data='ans:' + str(user_id) + ':9')
+    but10 = types.InlineKeyboardButton('10', callback_data='ans:' + str(user_id) + ':10')
     markup.add(but1, but2, but3, but4, but5, but6, but7, but8, but9, but10)
     bot.send_message(message_id, mes, reply_markup=markup)
 
-@bot.callback_query_handler(func=lambda call: True)
+
+@bot.callback_query_handler(func=lambda call: call.data.split(":")[0] == "ans")
 def querry_handler(call):
+    num_words_new = call.data.split(":")[2]
+    user_id_new = call.data.split(":")[1]
+    change_id_csv(num_words_new=num_words_new, user_id_new=user_id_new)
+    bot.edit_message_reply_markup(user_id_new, call.message.message_id, reply_markup=None)
+    bot.send_message(user_id_new, f'Вы выбрали {num_words_new} слов')
+
+
+def change_id_csv(num_words_new, user_id_new):
     temp = list()
-    num_words_new = call.data.split(":")[1]
-    user_id_new = call.data.split(":")[0]
     with open('id.csv', 'r', newline='', encoding='utf-8') as csv_file_r:
         reader = csv.DictReader(csv_file_r)
         for row in reader:
@@ -84,9 +107,13 @@ def choose_word():
         print(f'File does not exist. error - {err}')
         return None, None, None, None
 
+
 def main():
     english_word, russian_word1, russian_word2, russian_word3 = choose_word()
-    with open()
+    with open('id.csv', 'r', newline='', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            bot.send_message(row['user_id'], )
 
 
 if __name__ == '__main__':
